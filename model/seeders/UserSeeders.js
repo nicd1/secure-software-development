bf7f7217547a171a249f7f8cd6b613451d652286
nic <nicoladochnenko2@gmail.com>
2020-12-09 16:12:34 +0000

    added hashing/seeding back

--- model/seeders/UserSeeders.js
+++ model/seeders/UserSeeders.js
@@ -1,2 +1,22 @@
+const bcrypt = require('bcrypt');
 
+// Example seeded users - only for test and not following secure password practice.
+async function init(instance) {
+    await createUser(instance, 'nic', 'nic', '0001').catch(error => {});
+    await createUser(instance, 'user1', 'user1', '0002').catch(error => {});
+    await createUser(instance, 'admin', 'admin', '0001').catch(error => {});
+}
+
+async function addUser(instance, username, password, organisationId) {
+    const user = await this.addUser(username);
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(password, salt);
+
+    return await instance.createUser(username, hashed, organisationId);
+
+};
+
+exports = module.exports = {}
+exports.init = init;
+exports.addUser = addUser;
 
